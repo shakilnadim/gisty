3da64aa7f855a2f982d0a15ed10e5a5dcf74a47e
shakilnadim <shakilnadim@gmail.com>
2022-02-24 01:23:40 +0600

    write gist to a file

--- app/Commands/Get.php
+++ app/Commands/Get.php
@@ -3,10 +3,12 @@
 namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\File;
 use LaravelZero\Framework\Commands\Command;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use function Termwind\render;
 
 class Get extends Command
 {
@@ -25,6 +27,8 @@ class Get extends Command
     protected $description = 'Copies gist to file';
 
     private string $cacheKey = 'gists';
+    private array $gistList = [];
+    private string $gistFileName;
 
     /**
      * Execute the console command.
@@ -34,19 +38,13 @@ class Get extends Command
     public function handle(): void
     {
         if($this->option('cacheClear')) Cache::forget($this->cacheKey);
+
         $gists = $this->getGists();
-        $gistList = [];
-        $i = 1;
-        foreach ($gists as $gist) {
-            foreach ($gist['files'] as $file) {
-                $gistList[$i] = $file['filename'] . " | {$file['raw_url']}";
-                $i++;
-            }
-        }
-        $selectedGist = $this->choice('Select a gist', $gistList);
-        $selectedRawUrl = trim(explode('|', $selectedGist)[1]);
-        $gistBody = Http::get($selectedRawUrl)->body();
-        $this->info($gistBody);
+        $this->makeGistList($gists);
+        $gistBody = $this->getGistFromUser() . PHP_EOL;
+        $fileName = $this->getFileNameFromUser();
+        File::put($fileName, $gistBody);
+        render('<p class="bg-green p-1">Successful</p>');
     }
 
     public function getGists(): Collection
@@ -55,6 +53,42 @@ class Get extends Command
             $this->cacheKey,
             fn() => Http::withToken(env('GH_PERSONAL_ACCESS_TOKEN'))->get('https://api.github.com/gists')->collect()
         );
+    }
+
+    public function makeGistList(Collection $gists): void
+    {
+        $i = 1;
+        foreach ($gists as $gist) {
+            foreach ($gist['files'] as $file) {
+                $this->gistList[$i] = $file['filename'] . " | {$file['raw_url']}";
+                $i++;
+            }
+        }
+    }
+
+    public function getGistFromUser(): string
+    {
+        $selectedGist = $this->choice('Select a gist', $this->gistList);
+        $this->gistFileName = trim(explode('|', $selectedGist)[0]);
+        $gistRawUrl = trim(explode('|', $selectedGist)[1]);
+        return Http::get($gistRawUrl)->body();
+    }
+
+    private function getFileNameFromUser(): string
+    {
+        $path = getcwd() .'/'. $this->ask('input file name');
+        if ($this->isFileName($path)) {
+            File::ensureDirectoryExists(File::dirname($path));
+            return $path;
+        }
+        File::ensureDirectoryExists($path);
+        return $path.'/'.$this->gistFileName;
+    }
+
+    public function isFileName($path): bool
+    {
+        $splitPath = explode('/', $path);
+        return str_contains($splitPath[count($splitPath) - 1], '.');
     }
 
     /**
